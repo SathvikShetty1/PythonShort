561b25851ebe89e28e7f22aa3ec31e18c6ea533a
Sathvik Shetty <119489811+SathvikShetty1@users.noreply.github.com>
2023-09-07 13:07:43 +0530

    Queens.py

---
+++ Queens.py
@@ -0,0 +1,50 @@
+def is_safe(board, row, col, n):
+    # Check the column on top
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    # Check the upper-left diagonal
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check the upper-right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, n)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+def (n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
+
+    def solve(row):
+        if row == n:
+            for row in board:
+                print(" ".join(["Q" if cell == 1 else "." for cell in row]))
+            print()
+            return
+
+        for col in range(n):
+            if is_safe(board, row, col, n):
+                board[row][col] = 1
+                solve(row + 1)
+                board[row][col] = 0
+
+    solve(0)
+    
+n = int(input("Enter the no of queens "))
+solve_n_queens(n)
+"""
+Enter no of Queens: 4
+. . Q . 
+Q . . . 
+. . . Q 
+. Q . . 
+
+. Q . . 
+. . . Q 
+Q . . . 
+. . Q . 
+"""
